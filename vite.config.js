b628cd7f26627e1082af0670182b6b70c6695743
uviiii <175930735+Uviii9p@users.noreply.github.com>
2026-02-06 13:19:14 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -21,18 +21,21 @@ export default defineConfig({
         orientation: 'portrait',
         icons: [
           {
-            src: 'pwa-icon.svg',
+            src: '/pwa-icon.svg',
             sizes: '192x192',
             type: 'image/svg+xml',
             purpose: 'any maskable'
           },
           {
-            src: 'pwa-icon.svg',
+            src: '/pwa-icon.svg',
             sizes: '512x512',
             type: 'image/svg+xml',
             purpose: 'any maskable'
           }
         ]
+      },
+      devOptions: {
+        enabled: true
       }
     })
   ],
